0617854183f31eb147686a2e97b94a6dd443795a
Peter Zeng <zyfniu@hotmail.com>
2022-04-20 20:20:57 -0400

    Create demultiplex_helper.r

---
+++ multiseq_scripts/demultiplex_helper.r
@@ -0,0 +1,21 @@
+#!/usr/bin/env Rscript --vanilla
+###This is a helper script to run Chris Mcginis's multise demultiplex script https://github.com/chris-mcginnis-ucsf/MULTI-seq
+### run like this Rscript --vanilla /scratch/n/nicholsa/zyfniu/AN_WGS/multiseq_scripts/demultiplex_helper.r /path/read_1 /path/to/read_2
+### /path/to/barcodes.Rds /path/to/cell.id.vec.Rds /path/to/save/prefix
+
+library(deMULTIplex)
+args = commandArgs(trailingOnly=TRUE)
+
+# test if there are four arguments: if not, return an error
+if (length(args)!=5) {
+  stop("Four argument must be supplied in this order: ", call.=FALSE)
+}
+bar.ref <- readRDS(args[3])
+cell.id.vec <- readRDS(args[4])
+readTable <- MULTIseq.preProcess(R1 = args[1],
+                                 R2 = args[2],
+                                 cellIDs = cell.id.vec, cell=c(1,16), umi=c(17,28), tag=c(1,8)
+                                )
+saveRDS(readTable,paste0(args[5],"_readTable.rds"))
+bar.table <- MULTIseq.align(readTable, cell.id.vec, bar.ref)
+saveRDS(bar.table,paste0(args[5],"_barTable.rds"))
